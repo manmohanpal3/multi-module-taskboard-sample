0f89f159a39dc08067ae2196475d752a80490e57
Manmohan Singh Pal <manmohanpal@Manmohans-MacBook-Pro.local>
2025-11-18 22:43:23 +0530

    inital commit

---
+++ app/src/main/java/com/mini/taskboard/multi/module/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.mini.taskboard.multi.module
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mini.taskboard.multi.module.ui.theme.MultiModuleTaskBoardSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MultiModuleTaskBoardSampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MultiModuleTaskBoardSampleTheme {
+        Greeting("Android")
+    }
+}
